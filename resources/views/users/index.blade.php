4c8c7496c9364cee7a68b59831e9cfaa96766d2a
Muhammad Aatif Saeed <atifsaeed.5243@gmail.com>
2024-07-14 18:28:17 +0500

    language module added

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,7 +12,7 @@
                     <!-- Button trigger modal -->
                     <button type="button" class="btn btn-sm btn-primary userAdd" data-bs-toggle="modal"
                         data-bs-target="#addUserModel">
-                        {{ __('Add User') }}
+                        {{ __('messages.Add User') }}
                     </button>
 
                     <!-- Add User Modal -->
@@ -20,7 +20,7 @@
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel1">{{ __('Add User') }}</h5>
+                                    <h5 class="modal-title" id="exampleModalLabel1">{{ __('messages.Add User') }}</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                                 </div>
@@ -29,39 +29,39 @@
                                     <div class="modal-body">
                                         <div class="row">
                                             <div class="col mb-3">
-                                                <label for="name" class="form-label">{{ __('Name') }}</label>
+                                                <label for="name" class="form-label">{{ __('messages.Name') }}</label>
                                                 <input type="text" name="name" class="form-control"
-                                                    placeholder="{{ __('Enter Name') }}" required />
+                                                    placeholder="{{ __('messages.Enter Name') }}" required />
                                             </div>
                                         </div>
                                         <div class="row">
                                             <div class="col mb-3">
-                                                <label for="email" class="form-label">{{ __('Email') }}</label>
+                                                <label for="email" class="form-label">{{ __('messages.Email') }}</label>
                                                 <input type="email" name="email" class="form-control"
-                                                    placeholder="{{ __('Enter Email') }}" required />
+                                                    placeholder="{{ __('messages.Enter Email') }}" required />
                                             </div>
                                         </div>
                                         <div class="row">
                                             <div class="col mb-3">
-                                                <label for="password" class="form-label">{{ __('Password') }}</label>
+                                                <label for="password" class="form-label">{{ __('messages.Password') }}</label>
                                                 <input type="password" name="password" class="form-control"
-                                                    placeholder="{{ __('Enter Password') }}" required />
+                                                    placeholder="{{ __('messages.Enter Password') }}" required />
                                             </div>
                                         </div>
                                         <div class="col mb-3">
-                                            <label for="status" class="form-label">{{ __('Status') }}</label>
+                                            <label for="status" class="form-label">{{ __('messages.Status') }}</label>
 
                                             <select id="status" name="status" class="form-select form-select-lg">
-                                                <option value="Active">{{ __('Active') }}</option>
-                                                <option value="Inactive">{{ __('Inactive') }}</option>
+                                                <option value="Active">{{ __('messages.Active') }}</option>
+                                                <option value="Inactive">{{ __('messages.Inactive') }}</option>
                                             </select>
                                         </div>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                                            {{ __('Close') }}
+                                            {{ __('messages.Close') }}
                                         </button>
-                                        <button type="submit" class="btn btn-sm btn-primary">{{ __('Save changes') }}</button>
+                                        <button type="submit" class="btn btn-sm btn-primary">{{ __('messages.Save changes') }}</button>
                                     </div>
                                 </form>
                             </div>
@@ -72,7 +72,7 @@
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel1">{{ __('Edit User') }}</h5>
+                                    <h5 class="modal-title" id="exampleModalLabel1">{{ __('messages.Edit User') }}</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                                 </div>
@@ -82,39 +82,39 @@
                                     <div class="modal-body">
                                         <div class="row">
                                             <div class="col mb-3">
-                                                <label for="name" class="form-label">{{ __('Name') }}</label>
+                                                <label for="name" class="form-label">{{ __('messages.Name') }}</label>
                                                 <input type="text" id="name" name="name" class="form-control"
-                                                    placeholder="{{ __('Enter Name') }}" required />
+                                                    placeholder="{{ __('messages.Enter Name') }}" required />
                                             </div>
                                         </div>
                                         <div class="row">
                                             <div class="col mb-3">
-                                                <label for="email" class="form-label">{{ __('Email') }}</label>
+                                                <label for="email" class="form-label">{{ __('messages.Email') }}</label>
                                                 <input type="email" id="email" name="email" class="form-control"
-                                                    placeholder="{{ __('Enter Email') }}" required />
+                                                    placeholder="{{ __('messages.Enter Email') }}" required />
                                             </div>
                                         </div>
                                         <div class="row">
                                             <div class="col mb-3">
-                                                <label for="password" class="form-label">{{ __('Password') }}</label>
+                                                <label for="password" class="form-label">{{ __('messages.Password') }}</label>
                                                 <input type="password" id="password" name="password" class="form-control"
-                                                    placeholder="{{ __('Enter Password') }}" />
+                                                    placeholder="{{ __('messages.Enter Password') }}" />
                                             </div>
                                         </div>
                                         <div class="col mb-3">
-                                            <label for="status" class="form-label">{{ __('Status') }}</label>
+                                            <label for="status" class="form-label">{{ __('messages.Status') }}</label>
 
                                             <select id="status" name="status" class="form-select form-select-lg">
-                                                <option value="Active">{{ __('Active') }}</option>
-                                                <option value="Inactive">{{ __('Inactive') }}</option>
+                                                <option value="Active">{{ __('messages.Active') }}</option>
+                                                <option value="Inactive">{{ __('messages.Inactive') }}</option>
                                             </select>
                                         </div>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                                            {{ __('Close') }}
+                                            {{ __('messages.Close') }}
                                         </button>
-                                        <button type="submit" class="btn btn-sm btn-primary">{{ __('Save changes') }}</button>
+                                        <button type="submit" class="btn btn-sm btn-primary">{{ __('messages.Save changes') }}</button>
                                     </div>
                                 </form>
                             </div>
@@ -125,7 +125,7 @@
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel1">{{ __('Delete User') }}</h5>
+                                    <h5 class="modal-title" id="exampleModalLabel1">{{ __('messages.Delete User') }}</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                                 </div>
@@ -135,15 +135,15 @@
                                         <div class="row">
                                             <input type="hidden" name="id" id="UserIdDelete">
                                             <div class="col mb-3">
-                                                <p>{{ __('Are You Sure You Want to delete This User') }}</p>
+                                                <p>{{ __('messages.Are You Sure You Want to delete This User') }}</p>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                                            {{ __('Close') }}
+                                            {{ __('messages.Close') }}
                                         </button>
-                                        <button type="submit" class="btn btn-sm btn-danger">{{ __('Delete') }}</button>
+                                        <button type="submit" class="btn btn-sm btn-danger">{{ __('messages.Delete') }}</button>
                                     </div>
                                 </form>
                             </div>
@@ -153,15 +153,15 @@
             </div>
             <!-- Responsive Table -->
             <div class="card">
-                <h5 class="card-header">{{ __('Users') }}</h5>
+                <h5 class="card-header">{{ __('messages.Users') }}</h5>
                 <div class="table-responsive text-nowrap">
                     <table class="table">
                         <thead>
                             <tr class="text-nowrap">
-                                <th>{{ __('Name') }}</th>
-                                <th>{{ __('Email') }}</th>
-                                <th>{{ __('Status') }}</th>
-                                <th>{{ __('Action') }}</th>
+                                <th>{{ __('messages.Name') }}</th>
+                                <th>{{ __('messages.Email') }}</th>
+                                <th>{{ __('messages.Status') }}</th>
+                                <th>{{ __('messages.Action') }}</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -174,13 +174,13 @@
                                         <button type="button" data-id="{{ $user->id }}"
                                             class="btn btn-sm btn-primary editUserButton" data-bs-toggle="modal"
                                             data-bs-target="#editUserModel">
-                                            {{ __('Edit User') }}
+                                            {{ __('messages.Edit User') }}
                                         </button>
                                         <br>
                                         <button type="button" data-id="{{ $user->id }}"
                                             class="btn btn-sm btn-danger deleteUser" data-bs-toggle="modal"
                                             data-bs-target="#deleteUserModel">
-                                            {{ __('Delete User') }}
+                                            {{ __('messages.Delete User') }}
                                         </button>
                                     </td>
                             @endforeach
